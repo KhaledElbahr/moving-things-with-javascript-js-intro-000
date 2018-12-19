c96b8bcb1e77240cef407eb98fbea7a0f81eec1a
Khaled Elbahr <khaledelbahr2016@gmail.com>
2018-12-19 18:46:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ var dodger = document.getElementById('dodger');
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
- 
+
   if (left > 0) {
     dodger.style.left = `${left - 1}px`
   }
 }
 
 document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
+  if (e.which === 37) 
     moveDodgerLeft()
-  }
+    
+  if (e.which === 39)
+    moveDodgerRight()
 })
