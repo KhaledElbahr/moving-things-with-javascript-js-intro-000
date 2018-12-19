3887ceef976f99ce09761a41ddd8573a2f9335ab
Khaled Elbahr <khaledelbahr2016@gmail.com>
2018-12-19 19:10:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
 
-  if (left > 0) {
+  if (left > 0 && left < 360) {
     dodger.style.left = `${left - 1}px`
   }
 }
